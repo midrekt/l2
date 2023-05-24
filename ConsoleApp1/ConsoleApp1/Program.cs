c19d96d563032e1530a33c01d2e14bc594af48f7
midrekt <arenterwer@gmail.com>
2023-05-24 19:45:01 +0700

    start

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            char vib;
+
+            Console.WriteLine("Введите число А::");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите число В::");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите наименование оператора:");
+            vib = Convert.ToChar(Console.ReadLine());
+
+
+            switch (vib)
+            {
+                case '+':
+
+                    break;
+                case '-':
+
+                    break;
+                case '*':
+
+                    break;
+                case '/':
+
+                    break;
+                default:
+                    Console.WriteLine("Неизвестный оператор");
+                    break;
+            }
+        }
+    }
+}
